489940134f403031ed9ffe8e81f9cad209861e99
PaulVelthuis93 <paulvelthuis93@gmail.com>
2016-01-19 15:17:17 +0100

    bvg-api

--- server.js
+++ server.js
@@ -5,3 +5,11 @@
  */
 var app = require('./config/lib/app');
 var server = app.start();
+require('request');
+
+var request = require('request');
+request('https://bvg-api.herokuapp.com/station?input=alexanderplatz', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body) // Show the HTML for the Google homepage.
+  }
+});
