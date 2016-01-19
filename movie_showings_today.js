f61a4b27f9a960780cce37fd3562397ddae0ba30
andisadewi <andisa.dewi@gmail.com>
2016-01-19 13:57:34 +0100

    updated some info

--- movie_showings_today.js
+++ movie_showings_today.js
@@ -2,27 +2,33 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 
-request('http://www.google.de/movies?near=berlin', function (error, response, html) {
-  if (!error && response.statusCode == 200) {
-    var $ = cheerio.load(html);
-    $('div.theater').each(function(i, element){
-	 var desc = $(this).children('.desc');
- 	 var name = $(desc).children('.name');
-         console.log("Theater name: " + name.text());
-	 var address = $(desc).children('.info');
-	 console.log("Theater info: " + address.text());
-	
-	 var showtimes = $(this).children('.showtimes');
-	 console.log("Showings: ");
-	 $(showtimes).find('.name').each(function(i,elem){
-		console.log($(this).text());
-		var infos = $(this).next() ;
-		console.log(infos.text()) ;
-		var times = $(infos).next() ;
-	 	console.log(times.text());
-	 }) ;
-	 
-	 console.log("\n");
-     });
-  }
-});
+for(i = 0; i <= 70; i+=10) {
+	var link = "http://www.google.de/movies?near=berlin&start=" + i ; 
+	request(link, function (error, response, html) { 
+	if (!error && response.statusCode == 200) {
+	    var $ = cheerio.load(html);
+	    if ($('div.theater').length == 0){
+		return ;
+	    }
+	    $('div.theater').each(function(i, element){
+		 var desc = $(this).children('.desc');
+		 var name = $(desc).children('.name');
+		 console.log("Theater name: " + name.text());
+		 var address = $(desc).children('.info');
+		 console.log("Theater info: " + address.text());
+		
+		 var showtimes = $(this).children('.showtimes');
+		 console.log("Showings: ");
+		 $(showtimes).find('.name').each(function(i,elem){
+			console.log($(this).text());
+			var infos = $(this).next() ;
+			console.log(infos.text()) ;
+			var times = $(infos).next() ;
+			console.log(times.text());
+		 }) ;
+		 
+		 console.log("\n");
+	     });
+	  }
+	});
+}
