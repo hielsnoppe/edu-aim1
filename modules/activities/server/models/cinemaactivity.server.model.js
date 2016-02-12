497019e8088fd6660817643371aa6bd39d387a2d
andisadewi <andisa.dewi@gmail.com>
2016-02-13 00:38:15 +0100

    updated cinema schema

--- modules/activities/server/models/cinemaactivity.server.model.js
+++ modules/activities/server/models/cinemaactivity.server.model.js
@@ -13,24 +13,76 @@ var ActivitySchema = require('./activity.server.model.js').schema;
  * Movie Schema
  */
 var CinemaActivitySchema = ActivitySchema.extend({
-  title: {
-    type: String,
-    default: '',
-    trim: true,
-    required: 'Title cannot be blank'
-  },
-  infos: {
-    type: String,
-    default: '',
+  time: {
+    type: Date,
+    default: Date.now,
     trim: true
   },
-  times: {
-    type: String,
-    default: '',
+  date: {
+    type: Date,
+    default: Date.now,
     trim: true
+  },
+  theater: {
+    name: {
+      type: String,
+      default: '',
+      trim: true,
+      required: 'Name cannot be blank'
+    },
+    address: {
+      type: String,
+      default: '',
+      trim: true
+    }
   },
   movie: {
-    type: Schema.Types.Mixed
+    title: {
+      type: String,
+      default: '',
+      trim: true,
+      required: 'Title cannot be blank'
+    },
+    releaseDate: {
+      type: Date,
+      default: Date.now,
+      trim: true
+    },
+    adult: {
+      type: Boolean,
+      default: false,
+      trim: true
+    },
+    plot: {
+      type: String,
+      default: '',
+      trim: true
+    },
+    runtime: {
+      type: Number,
+      default: 0,
+      trim: true
+    },
+    genres: [ {name : {
+                type: String,
+                default: '',
+                trim: true
+             }}],
+    countries: [ {name : {
+                type: String,
+                default: '',
+                trim: true
+                }}],
+    director: {
+      type: String,
+      default: '',
+      trim: true
+    },
+    cast: [ {name : {
+                type: String,
+                default: '',
+                trim: true
+          }}]
   }
 });
 
